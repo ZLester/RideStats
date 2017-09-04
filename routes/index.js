1f279917ec4b955fba0510f582df220ef92823b1
Zachary Lester <zlester@rubiconproject.com>
2017-09-04 00:06:15 -0700

    Update middleware and reroutes

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,10 @@ const uberRouter = require('../resources/uber/uber-router');
 const userRouter = require('../resources/users/user-router');
 const authRouter = require('../resources/auth/auth-router');
 
-const uberClient = require('../resources/uber/uber-client');
-
 const requireAuthentication = require('../middleware/require-authentication');
 
 module.exports = (app) => {
     app.use('/api/uber', requireAuthentication, uberRouter);
     app.use('/api/users', userRouter);
     app.use('/api/auth', authRouter);
-    app.use('/', (req, res) => res.send(`<a href="${uberClient.uber_login_endpoint}">Log In</a><br /><a href="/api/auth/logout">Log Out</a>`));
 };
