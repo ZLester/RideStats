dd871b50c97893f259049b5f96fdeb34db2bc4fb
Zachary Lester <zlester@rubiconproject.com>
2017-09-04 14:39:16 -0700

    Add cors

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,9 @@
 const session = require('./session');
 const logger = require('./logger');
+const cors = require('cors')();
 
 module.exports = (app) => {
+    app.use(cors);
     app.use(logger);
     app.use(session);
 };
