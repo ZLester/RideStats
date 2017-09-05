537cc9533496c48ef8dd3d12a6f971d4d1b4de25
Zachary Lester <zlester@rubiconproject.com>
2017-09-04 17:48:55 -0700

    Remove uneeded middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,7 @@
 const session = require('./session');
 const logger = require('./logger');
-const cors = require('cors')();
 
 module.exports = (app) => {
-    app.use(cors);
     app.use(logger);
     app.use(session);
 };
